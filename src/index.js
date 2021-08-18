f743423e576b6cf7e8360dc20f006dc6e72b5b33
lukasmd1989 <lukas.m.dittmer@me.com>
2021-08-19 00:19:39 +0200

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
-import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
+//import "./style.css";
+//import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 import App from "./App2";
